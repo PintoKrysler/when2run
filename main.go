3be12a87b38902d08d4fe0189a2f37f9405cdad5
Krysler pinto <pinto.krysler@gmail.com>
2017-04-29 13:59:59 -0600

    Added UI to the project, still working on displaying information received from the weather API

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+var tpl *template.Template
+
+//"encoding/json"
+// "fmt"
+// "io/ioutil"
+// "log"
+// "net/http"
 
 //ResponseMain ...
 type responseMain struct {
@@ -26,12 +35,113 @@ type responsetype struct {
 	List []responseElem `json:"list"`
 }
 
+type tplData struct {
+	Title     string
+	FirstName string
+	TabActive string
+	Data      responsetype
+}
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
 func main() {
-	fmt.Println("This is a test")
+
+	http.HandleFunc("/account", accountHandler)
+	http.HandleFunc("/settings", settingsHandler)
+	http.HandleFunc("/createUser", createUserHandler)
+	http.HandleFunc("/setSettings", setSettingsHandler)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", indexHandler)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	templateData := tplData{
+		Title:     "Index",
+		TabActive: "index",
+	}
+
+	err := tpl.ExecuteTemplate(w, "index.gohtml", templateData)
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error - Resource not found", http.StatusInternalServerError)
+	}
+
+}
+
+func settingsHandler(w http.ResponseWriter, req *http.Request) {
+	templateData := tplData{
+		Title:     "Settings",
+		TabActive: "settings",
+	}
+	err := tpl.ExecuteTemplate(w, "settings.gohtml", templateData)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func accountHandler(w http.ResponseWriter, req *http.Request) {
+	templateData := tplData{
+		Title:     "Account",
+		TabActive: "account",
+	}
+	err := tpl.ExecuteTemplate(w, "account.gohtml", templateData)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createUserHandler(w http.ResponseWriter, req *http.Request) {
+	templateData := tplData{
+		Title:     "Account",
+		TabActive: "account",
+	}
+
+	// post request, http.MethodPost is a constant
+	if req.Method == http.MethodPost {
+		userName := req.FormValue("uname")
+		templateData.FirstName = userName
+		err := tpl.ExecuteTemplate(w, "account.gohtml", templateData)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func setSettingsHandler(w http.ResponseWriter, req *http.Request) {
+	templateData := tplData{
+		Title:     "Your Running Times",
+		TabActive: "times",
+	}
+
+	// post request, http.MethodPost is a constant
+	if req.Method == http.MethodPost {
+		// minTemp := req.FormValue("minTemp")
+		// maxTemp := req.FormValue("maxTemp")
+		var data = makeWeatherAPIcall()
+		fmt.Println("Data", data)
+		templateData.Data = data
+
+		err := tpl.ExecuteTemplate(w, "times.gohtml", templateData)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func makeWeatherAPIcall() responsetype {
 	apiKey := "4793867f02934a10b3033be4d68f385c"
 	baseURL := "http://api.openweathermap.org/data/2.5/forecast?q=lakewood,co"
 	query := baseURL + "&appid=" + apiKey + "&id=5427946"
-	fmt.Println(query)
+
 	res, err := http.Get(query)
 	if err != nil {
 		log.Fatal(err)
@@ -45,6 +155,5 @@ func main() {
 	var r = responsetype{}
 	json.Unmarshal(response, &r)
 
-	// Parse JSON
-	fmt.Println(r.List[0])
+	return r
 }
